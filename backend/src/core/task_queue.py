0807acb79a3bc5dc71aa8f7c736d1b214da2f6d9
agrimsri <agrimsri26@gmail.com>
2026-05-16 08:10:32 +0000

    Cleared old files

--- backend/src/core/task_queue.py
+++
@@ -1,7 +0,0 @@
-from rq import Queue
-from src.core.redis_client import get_redis_connection
-
-
-def get_queue():
-    redis_conn = get_redis_connection()
-    return Queue("default", connection=redis_conn)
